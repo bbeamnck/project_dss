258e1ed56adba720d1a1edd75a8fea9f90c35013
AryMiku <b_wasupon@hotmail.com>
2019-04-11 01:21:25 +0700

    แก้ font

--- form.php
+++ form.php
@@ -12,12 +12,7 @@
     <link rel="stylesheet" href="css/beamstyle.css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
         crossorigin="anonymous"></script>
-        <style>
-        @font-face {
-                font-family: "Waffle";
-                src: url(Waffle.otf);
-            }
-        </style> 
+        
 
 </head>
 <!-- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> -->
